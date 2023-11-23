69c70cc6465a164aeafe21a86dd3500f7cbcc6fe
Abul2070 <148291505+Abul2070@users.noreply.github.com>
2023-11-23 22:13:38 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,233 @@
+        
+        function redirect(){
+            var test =  document.querySelector('#passport');
+            if(test.value == "B00565310"){
+                    window.location.href="elias.html"
+            }
+                else if(test.value == "EL0058969"){
+                     window.location.href="akter.html"
+                }
+                    else if(test.value == "EL0812469"){
+                     window.location.href="rajon.html"
+                }
+                             else if(test.value == "EL0093746"){
+                     window.location.href="mosharof.html"
+                }
+                                 else if(test.value == "A11581089"){
+                     window.location.href="nadim.html"
+                } 
+                                 else if(test.value == "BY0819559"){
+                     window.location.href="bellal.html"
+                }
+                                      else if(test.value == "EF0856068"){
+                     window.location.href="jamal.html"
+                }
+                                          else if(test.value == "EL0867812"){
+                     window.location.href="musfiq.html"
+                }
+                                               else if(test.value == "BK0075371"){
+                     window.location.href="rabiul.html"
+                }
+                                                    else if(test.value == "A04923234"){
+                     window.location.href="prodan.html"
+                }
+                                                         else if(test.value == "A02241903"){
+                     window.location.href="ajikur.html"
+                }
+                                                              else if(test.value == "A00487851"){
+                     window.location.href="sohag.html"
+                }
+                                                                   else if(test.value == "EK0929500"){
+                     window.location.href="rabbani.html"
+                }
+                                                                       else if(test.value == "EJ0252795"){
+                     window.location.href="sami.html"
+                }
+                                                                           else if(test.value == "EH0231598"){
+                     window.location.href="rasel.html"
+                }
+                                                                               else if(test.value == "EK0187677"){
+                     window.location.href="ramjan.html"
+                }
+                                                                                   else if(test.value == "EL0623865"){
+                     window.location.href="imran.html"
+                }
+                                                                                        else if(test.value == "EA0072336"){
+                     window.location.href="nazimuddin.html"
+                }
+                                                                                            else if(test.value == "EG0416275"){
+                     window.location.href="mansura.html"
+                }
+                                                                                                else if(test.value == "A11588520"){
+                     window.location.href="sakil.html"
+                }
+                                                                                                    else if(test.value == "A12229934"){
+                     window.location.href="siyam.html"
+                }
+                                                                                                         else if(test.value == "EB0027265"){
+                     window.location.href="rakibul.html"
+                }
+                                                                                                              else if(test.value == "EG0366412"){
+                     window.location.href="billal.html"
+                }
+                                                                                                                   else if(test.value == "A02340939"){
+                     window.location.href="ripon.html"
+                }
+                                                                                                                        else if(test.value == "EG0810304"){
+                     window.location.href="thaher.html"
+                }
+                                                                                                                            else if(test.value == "EL0798530"){
+                     window.location.href="babul.html"
+                }
+                                                                                                                                else if(test.value == "EL0179019"){
+                     window.location.href="tufajjal.html"
+                }
+                                                                                                                                     else if(test.value == "EK0614874"){
+                     window.location.href="joynal.html"
+                }
+                                                                                                                                         else if(test.value == "EE00106617"){
+                     window.location.href="monir.html"
+                }
+                                                                                                                                             else if(test.value == "A02758621"){
+                     window.location.href="jobayar.html"
+                }
+                                                                                                                                                  else if(test.value == "EK0042565"){
+                     window.location.href="kawsar.html"
+                }
+                    else if(test.value == "EK0079225"){
+                     window.location.href="juwel.html"
+                }
+                        else if(test.value == "R1695889"){
+                     window.location.href="kalyan.html"
+                }
+                     else if(test.value == "EJ0968523"){
+                     window.location.href="hossain.html"
+                }
+                         else if(test.value == "EJ0794879"){
+                     window.location.href="kasem.html"
+                }
+                               else if(test.value == "EB0334698"){
+                     window.location.href="basher.html"
+                }
+                                   else if(test.value == "EJ0874168"){
+                     window.location.href="sakil2.html"
+                }
+                                       else if(test.value == "A00199777"){
+                     window.location.href="shahin.html"
+                }
+                                           else if(test.value == "EK0204071"){
+                     window.location.href="khaled.html"
+                }
+                                               else if(test.value == "EJ0175746"){
+                     window.location.href="sumon.html"
+                }
+                                                    else if(test.value == "A00520604"){
+                     window.location.href="mahabul.html"
+                }
+                                                          else if(test.value == "EK0232177"){
+                     window.location.href="ashidul.html"
+                }
+                                                                else if(test.value == "BN0502262"){
+                     window.location.href="afzal.html"
+                }
+                                                                    else if(test.value == "EL0301540"){
+                     window.location.href="nazrul.html"
+                }
+                                                                          else if(test.value == "EJ0488770"){
+                     window.location.href="amir.html"
+                }
+                                                                                else if(test.value == "BX0473517"){
+                     window.location.href="rasel_miah.html"
+                }
+                    else if(test.value == "B00748547"){
+                     window.location.href="adom.html"
+                }
+                     else if(test.value == "A07080315"){
+                     window.location.href="sakib.html"
+                }
+                         else if(test.value == "EL0170092"){
+                     window.location.href="kamal_khan.html"
+                }
+                              else if(test.value == "BY0745970"){
+                     window.location.href="salam.html"
+                }
+                                    else if(test.value == "A04938137"){
+                     window.location.href="mohan.html"
+                }
+                                        else if(test.value == "BY0590946"){
+                     window.location.href="akash.html"
+                }
+                                             else if(test.value == "A03627150"){
+                     window.location.href="liton.html"
+                }
+                                                 else if(test.value == "A03627151"){
+                     window.location.href="amer.html"
+                }
+                                                      else if(test.value == "B00599769"){
+                     window.location.href="rakib.html"
+                }
+                                                           else if(test.value == "A01987484"){
+                     window.location.href="shahajahan.html"
+                }
+                                                                else if(test.value == "EH0851615"){
+                     window.location.href="shopan.html"
+                }
+                                                                    else if(test.value == "EJ0557097"){
+                     window.location.href="subrata.html"
+                }
+                                                                        else if(test.value == "A00727349"){
+                     window.location.href="aminur.html"
+                }
+                                                                         else if(test.value == "EJ0846527"){
+                     window.location.href="rajib.html"
+                }
+                                                                         else if(test.value == "A07049942"){
+                     window.location.href="rasel3.html"
+                }
+                                                                         else if(test.value == "EL0759340"){
+                     window.location.href="tarek.html"
+                }
+                                                                             else if(test.value == "EL0236111"){
+                     window.location.href="shahed.html"
+                }
+                                                                                  else if(test.value == "A11227117"){
+                     window.location.href="juel.html"
+                }
+                                    else if(test.value == "EF0169938"){
+                     window.location.href="younus.html"
+                }
+                                         else if(test.value == "EE0843901"){
+                     window.location.href="ismail.html"
+                }
+                                             else if(test.value == "EE0759339"){
+                     window.location.href="hannan.html"
+                }
+                                                  else if(test.value == "BP0634411"){
+                     window.location.href="nur.html"
+                }
+                                                      else if(test.value == "EH0955352"){
+                     window.location.href="idris.html"
+                }
+                                                           else if(test.value == "BW0314820"){
+                     window.location.href="hoquee.html"
+                }
+                                                               else if(test.value == "B00145555"){
+                     window.location.href="selim_rega.html"
+                }
+                                        else if(test.value == "EH0784356"){
+                     window.location.href="kamal.html"
+                }
+                   else if(test.value == "A04279673"){
+                     window.location.href="kador.html"
+                }                                           
+            else{
+                 alert("invalid Passport")
+            }
+   
+              }
+           
+               
+             
+           
+         
+          
